1f10c7a1deedda0d2938a25a0282864bc26fb014
Kundan Dalmia <kundan.d.2023@mitb.smu.edu.sg>
2025-02-16 03:46:29 +0800

    publishing branch on user registration

---
+++ app/main.py
@@ -0,0 +1,56 @@
+import logging
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import JSONResponse
+from fastapi.security import HTTPBearer
+#from app.routes import books, ai, chroma, reviews
+from app.exceptions import ServiceException
+
+# Uncomment this line to include the Auth router
+from app.routes import auth
+
+security = HTTPBearer()
+
+app = FastAPI(
+    title="Recipe App",
+    description="An application to quickly give recipes based on avaialble ingredients",
+    version="1.0.0",
+)
+
+# Setup logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Global exception handler for HTTP exceptions
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request: Request, exc: HTTPException):
+    logger.warning(f"HTTP exception occurred: {str(exc)}")
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail": exc.detail},
+    )
+
+# Global exception handler for custom exceptions
+@app.exception_handler(ServiceException)
+async def http_exception_handler(request: Request, exc: HTTPException):
+    logger.warning(f"Service exception occurred: {str(exc)}")
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail": exc.detail},
+    )
+
+# Global exception handler for unhandled exceptions
+@app.exception_handler(Exception)
+async def global_exception_handler(request: Request, exc: Exception):
+    logger.error(f"Unhandled exception occurred: {str(exc)}")
+    return JSONResponse(
+        status_code=500,
+        content={"detail": "An unexpected error occurred"}
+    )
+
+# Include routes
+# app.include_router(books.router, prefix="/books", tags=["Books"])
+# app.include_router(reviews.router, prefix="", tags=["Reviews"])
+# app.include_router(ai.router, prefix="/ai", tags=["AI"])
+# app.include_router(chroma.router, prefix="/chroma", tags=["ChromaDB"])
+# Uncomment this line to include the Auth router
+app.include_router(auth.router, prefix="", tags=["Auth"])
