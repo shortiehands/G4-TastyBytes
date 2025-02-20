7ae2fdd90d922ee6a8aec3a9a072cbbfa37c9bc4
dhree03 <dhreeti.s.2023@smu.edu.sg>
2025-02-20 16:43:28 +0800

    update db to move from local sqllite to postgres on AWS RDS

--- app/db/session.py
+++ app/db/session.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./app.db"
+DATABASE_URL = "postgresql://postgres:Github2024#@database-recipe-instance-1.chqss26iwn5v.ap-southeast-1.rds.amazonaws.com/dbrecipe"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
