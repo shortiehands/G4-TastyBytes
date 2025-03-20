6a1843057ba78d83f6ad98b9d6bf86d33f298211
dhree03 <dhreeti.s.2023@smu.edu.sg>
2025-03-20 18:13:59 +0800

    changes database to a free one lmao

--- app/db/session.py
+++ app/db/session.py
@@ -1,7 +1,18 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://postgres:Github2024#@database-recipe-instance-1.chqss26iwn5v.ap-southeast-1.rds.amazonaws.com/dbrecipe"
+load_dotenv()
+
+# Read DB credentials from environment variables
+DATABASE_USER = os.getenv("DATABASE_USER")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+DATABASE_HOST = os.getenv("DATABASE_HOST")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+DATABASE_PORT = os.getenv("DATABASE_PORT", "3306")  # Default MySQL port
+
+DATABASE_URL = f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
