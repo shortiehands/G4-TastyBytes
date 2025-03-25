fbe4a043eb16c4c4639edfa1f8fc388b82d708da
ziqi-wong <ziqi.wong.2023@smu.edu.sg>
2025-03-25 21:48:07 +0800

    seperate front-end

--- app/dependencies/auth.py
+++
@@ -1,4 +0,0 @@
-from app.services.cognito_service import RoleChecker, CognitoAdminRole, CognitoUserRole
-
-required_admin_role = RoleChecker(CognitoAdminRole)
-required_user_role = RoleChecker(CognitoUserRole)
--- app/schema/llm.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class LLMRecipeResponse(BaseModel):
-    recipe_name: str
-    overview: str
-    ingredients: list
-    steps: list
